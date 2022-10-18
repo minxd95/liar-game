0eab65a963b42fff0348b5d9600c8942c8746e5e
minxd95 <newobj95@gmail.com>
2022-10-18 18:11:33 +0900

    emotion 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
